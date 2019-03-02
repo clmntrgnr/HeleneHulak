41a11182e7c97da8d81e154fbbd6a3bb0737f222
laetitiapot <potlaetitia@gmail.com>
2019-03-02 19:56:07 +0100

    article: layout

---
+++ wp-content/themes/gridsby-child/content-single.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @package gridsby
+ */
+?>
+
+	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	
+    	<header class="entry-header">
+			<?php the_title( '<h1 class="entry-title"><span class="title">', '</span></h1>' ); ?> 
+		</header><!-- .entry-header -->
+
+		<div class="entry-content">
+			<?php the_content(); ?>
+			<!-- <div class="entry-meta"> -->
+				<!-- <span class="meta-block"> <?php the_category(); ?></span> -->
+				<!-- <span class="meta-block"> <?php the_tags(); ?></span> -->
+			<!-- </div> -->
+			<!-- .entry-meta -->
+		</div><!-- .entry-content -->
+
+	</article><!-- #post-## -->
