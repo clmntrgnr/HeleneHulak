c8960d74e66e8c41c96abd0ad7dff78e1de9bf9c
laetitiapot <potlaetitia@gmail.com>
2019-03-02 18:21:23 +0100

    footer: add custom footer

---
+++ wp-content/themes/gridsby-child/footer.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after
+ *
+ * @package gridsby
+ */
+?>
+
+	</section><!-- #content --> 
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		
+        <div class="site-info"> 
+        
+        <?php if( get_theme_mod( 'active_footer_social' ) == '') : ?> 
+        	 
+                       
+        	<?php get_template_part( 'content', 'social' ); // Social Icons ?>
+          
+			 
+		<?php endif; ?>
+			
+        
+		<?php if ( get_theme_mod( 'gridsby_footer_phone' ) ) : ?> 
+        	<h3 class="phone"><?php echo esc_html( get_theme_mod( 'gridsby_footer_phone' )); ?></h3>
+        <?php endif; ?>
+        
+        <?php if ( get_theme_mod( 'gridsby_footer_contact' ) ) : ?>
+        	<h3 class="email"><a href="mailto:<?php echo esc_html( get_theme_mod( 'gridsby_footer_contact' )); ?>" target="_blank"><?php echo esc_html( get_theme_mod( 'gridsby_footer_contact' )); ?></a></h3> 
+        <?php endif; ?> 
+        </div><!-- .site-info -->
+	</footer><!-- #colophon -->
+
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
