a545a01f926aca13e390d05ccebe724433edce7d
laetitiapot <potlaetitia@gmail.com>
2019-04-24 22:38:22 +0200

    content: handle all fields in the template

--- wp-content/themes/gridsby-child/content.php
+++ wp-content/themes/gridsby-child/content.php
@@ -7,4 +7,7 @@
 
 	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 		<?php echo get_field('title'); ?>
+		<?php echo get_field('date'); ?>
+		<?php echo get_field('place'); ?>
+		<?php echo get_field('authors'); ?>
 	</article><!-- #post-## -->
