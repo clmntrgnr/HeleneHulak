de074f123a02634b304df754a798295c9243a9fc
laetitiapot <potlaetitia@gmail.com>
2019-05-01 13:18:50 +0200

    CV: add new template for CV

---
+++ wp-content/themes/gridsby-child/page-left-sidebar.php
@@ -0,0 +1,13 @@
+<?php
+/**
+Template Name: Left Sidebar
+ *
+ * @package gridsby
+ */
+
+get_header(); ?>
+
+<div class="grid grid-pad">
+<h1>Coucoucou de depuis le template</h1>
+</div>
+<?php get_footer(); ?>
