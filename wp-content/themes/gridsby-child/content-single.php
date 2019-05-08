1d33e2e9c9b6c9ec12134f7551403cfa92d263ad
laetitiapot <potlaetitia@gmail.com>
2019-05-08 15:40:02 +0200

    project: add modal

--- wp-content/themes/gridsby-child/content-single.php
+++ wp-content/themes/gridsby-child/content-single.php
@@ -15,31 +15,25 @@
 
 			<div class="HH-images">
 					<?php
+						$i = 0;
 						// check if the repeater field has rows of data
 						if( have_rows('images') ):
 							// loop through the rows of data
-								while ( have_rows('images') ) : the_row(); ?>
+								while ( have_rows('images') ) : $i++; the_row(); ?>
 										<div class="HH-image">
-											<div data-toggle="modal" data-target="#myModal">
+											<div data-toggle="modal" data-target="#myModal<?php echo $i;?>">
 												<img src=<?php the_sub_field('image') ?> />												
 											</div>
 										</div>
-								<?php endwhile;
-						endif;
-					?>
-					<?php
-						// check if the repeater field has rows of data
-						if( have_rows('images') ):
-							// loop through the rows of data
-								while ( have_rows('images') ) : the_row(); ?>
-										<!-- Modal -->
-										<!-- <div class="modal fade" id="myModal" role="dialog">
-											<div class="modal-dialog">
-												<div class="modal-content">
-													<img class="HH-image-full" src=<?php the_sub_field('image') ?> />												
-												</div>
-											</div>
-										</div> -->
+															<!-- Modal -->
+					<div class="modal fade" id="myModal<?php echo $i;?>" role="dialog">
+						<div class="modal-dialog">
+							<!-- Modal content-->
+							<div class="modal-content">
+								<img class="HH-image-full" src=<?php the_sub_field('image') ?> />												
+							</div>
+						</div>
+					</div>
 								<?php endwhile;
 						endif;
 					?>
